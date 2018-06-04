315870da3ee15094de77f35734deea3f79809b69
BlivionIaG <kev29lt@gmail.com>
2018-06-04 09:56:35 +0200

    modifié :         interfaceWeb/burgerquiz.sql

--- interfaceWeb/burgerquiz.sql
+++ interfaceWeb/burgerquiz.sql
@@ -19,46 +19,19 @@ DROP TABLE if exists Reponse;
 DROP TABLE if exists Partie;
 DROP TABLE if exists Utilisateur;
 DROP TABLE if exists comprend;
-DROP TABLE if exists possede;
+DROP TABLE if exists Possede;
 
 #------------------------------------------------------------
-# Table: Themes
+# Table: Utilisateur
 #------------------------------------------------------------
 
-CREATE TABLE Themes(
-        id_theme  Int  Auto_increment  NOT NULL ,
-        nom_theme Varchar (256)
-	,CONSTRAINT Themes_PK PRIMARY KEY (id_theme)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Question
-#------------------------------------------------------------
-
-CREATE TABLE Question(
-        id_question Int  Auto_increment  NOT NULL ,
-        choix_un    Varchar (256) ,
-        choix_deux  Varchar (256) ,
-        id_theme    Int NOT NULL
-	,CONSTRAINT Question_PK PRIMARY KEY (id_question)
-
-	,CONSTRAINT Question_Themes_FK FOREIGN KEY (id_theme) REFERENCES Themes(id_theme)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Reponse
-#------------------------------------------------------------
-
-CREATE TABLE Reponse(
-        id_reponse  Int  Auto_increment  NOT NULL ,
-        reponse     Varchar ,
-        proposition Varchar (512) ,
-        id_question Int NOT NULL
-	,CONSTRAINT Reponse_PK PRIMARY KEY (id_reponse)
-
-	,CONSTRAINT Reponse_Question_FK FOREIGN KEY (id_question) REFERENCES Question(id_question)
+CREATE TABLE Utilisateur(
+        id_utilisateur Int  Auto_increment  NOT NULL ,
+        mail           Varchar (256) NOT NULL ,
+        nom            Varchar (128) NOT NULL ,
+        prenom         Varchar (128) NOT NULL ,
+        mdp            Varchar (128) NOT NULL
+	,CONSTRAINT Utilisateur_PK PRIMARY KEY (id_utilisateur)
 )ENGINE=InnoDB;
 
 
@@ -68,22 +41,65 @@ CREATE TABLE Reponse(
 
 CREATE TABLE Partie(
         id_partie  Int  Auto_increment  NOT NULL ,
-        nom_partie Varchar
+        nom_partie Varchar (256) NOT NULL
 	,CONSTRAINT Partie_PK PRIMARY KEY (id_partie)
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: Utilisateur
+# Table: Theme
 #------------------------------------------------------------
 
-CREATE TABLE Utilisateur(
-        id_utilisateur Int  Auto_increment  NOT NULL ,
-        mail           Varchar (256) ,
-        prenom         Varchar (128) ,
-        nom            Varchar (128) ,
-        mdp            Varchar (64)
-	,CONSTRAINT Utilisateur_PK PRIMARY KEY (id_utilisateur)
+CREATE TABLE Theme(
+        id_theme  Int  Auto_increment  NOT NULL ,
+        nom_theme Varchar (256) NOT NULL
+	,CONSTRAINT Theme_PK PRIMARY KEY (id_theme)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Question
+#------------------------------------------------------------
+
+CREATE TABLE Question(
+        id_question Int  Auto_increment  NOT NULL ,
+        choix_un    Varchar (256) NOT NULL ,
+        choix_deux  Varchar (256) NOT NULL ,
+        id_theme    Int NOT NULL
+	,CONSTRAINT Question_PK PRIMARY KEY (id_question)
+
+	,CONSTRAINT Question_Theme_FK FOREIGN KEY (id_theme) REFERENCES Theme(id_theme)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Reponse
+#------------------------------------------------------------
+
+CREATE TABLE Reponse(
+        id_reponse     Int  Auto_increment  NOT NULL ,
+        valeur_reponse Varchar (128) NOT NULL ,
+        proposition    Varchar (512) NOT NULL ,
+        id_question    Int NOT NULL
+	,CONSTRAINT Reponse_PK PRIMARY KEY (id_reponse)
+
+	,CONSTRAINT Reponse_Question_FK FOREIGN KEY (id_question) REFERENCES Question(id_question)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Possede
+#------------------------------------------------------------
+
+CREATE TABLE Possede(
+        id_partie      Int NOT NULL ,
+        id_utilisateur Int NOT NULL ,
+        score          BigInt NOT NULL ,
+        temps          BigInt NOT NULL
+	,CONSTRAINT Possede_PK PRIMARY KEY (id_partie,id_utilisateur)
+
+	,CONSTRAINT Possede_Partie_FK FOREIGN KEY (id_partie) REFERENCES Partie(id_partie)
+	,CONSTRAINT Possede_Utilisateur0_FK FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
 )ENGINE=InnoDB;
 
 
@@ -92,27 +108,11 @@ CREATE TABLE Utilisateur(
 #------------------------------------------------------------
 
 CREATE TABLE comprend(
-        id_partie   Int NOT NULL ,
-        id_question Int NOT NULL
-	,CONSTRAINT comprend_PK PRIMARY KEY (id_partie,id_question)
+        id_question Int NOT NULL ,
+        id_partie   Int NOT NULL
+	,CONSTRAINT comprend_PK PRIMARY KEY (id_question,id_partie)
 
-	,CONSTRAINT comprend_Partie_FK FOREIGN KEY (id_partie) REFERENCES Partie(id_partie)
-	,CONSTRAINT comprend_Question0_FK FOREIGN KEY (id_question) REFERENCES Question(id_question)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: possede
-#------------------------------------------------------------
-
-CREATE TABLE possede(
-        id_partie      Int NOT NULL ,
-        id_utilisateur Int NOT NULL ,
-        score          BigInt NOT NULL ,
-        temps          BigInt NOT NULL
-	,CONSTRAINT possede_PK PRIMARY KEY (id_partie,id_utilisateur)
-
-	,CONSTRAINT possede_Partie_FK FOREIGN KEY (id_partie) REFERENCES Partie(id_partie)
-	,CONSTRAINT possede_Utilisateur0_FK FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+	,CONSTRAINT comprend_Question_FK FOREIGN KEY (id_question) REFERENCES Question(id_question)
+	,CONSTRAINT comprend_Partie0_FK FOREIGN KEY (id_partie) REFERENCES Partie(id_partie)
 )ENGINE=InnoDB;
 
