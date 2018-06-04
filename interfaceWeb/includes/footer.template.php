0033dc006d03cf63f319bf8359fa196966419b0f
keskydi <antoine.orhant@gmail.com>
2018-06-04 10:22:31 +0200

    footer and header + css bootstrap

---
+++ interfaceWeb/includes/footer.template.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('ENVIRONMENT')) { exit(); } // On empêche l'accès direct en HTTP
+?>
+
+<footer class="sticky-footer">
+      <div class="container">
+<div id="footer">
+    <span style="float:left;">Last Updated: October 15, 2012</span>
+    <span style="float:right;">Contact Us  Login</span>
+</div>
+      </div>
+    </footer>
+
