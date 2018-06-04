eedfb62613b6047b6afe77da0aa843e3568d37a9
BlivionIaG <kev29lt@gmail.com>
2018-06-04 17:23:08 +0200

    Mise à jour interface BDD PHP

--- interfaceWeb/php/InterfaceBDD.php
+++ interfaceWeb/php/InterfaceBDD.php
@@ -13,6 +13,8 @@
  */
 require_once('consts.php');
 require_once('Utilisateur.php');
+require_once('Theme.php');
+require_once('Partie.php');
 
 class InterfaceBDD {
 
@@ -70,12 +72,29 @@ class InterfaceBDD {
         }
         return $result;
     }
-    
-     public function deleteUser($id) {
-         try {
+
+    public function UpdateUser($user) {
+        try {
+            $request = 'update Utilisateur mail=:mail, prenom=:prenom, nom=:nom, mdp=:mdp where set id_utilisateur=:id';
+            $statement = $this->getBdd()->prepare($request);
+            $statement->bindParam(':id', $user->getId_utilisateur(), PDO::PARAM_INT);
+            $statement->bindParam(':mail', $user->getMail(), PDO::PARAM_STR, 256);
+            $statement->bindParam(':prenom', $user->getPrenom(), PDO::PARAM_STR, 128);
+            $statement->bindParam(':nom', $user->getNom(), PDO::PARAM_STR, 128);
+            $statement->bindParam(':mdp', $user->getMdp(), PDO::PARAM_STR, 128);
+            $result = $statement->execute();
+        } catch (PDOException $exception) {
+            error_log('Connection error: ' . $exception->getMessage());
+            return false;
+        }
+        return $result;
+    }
+
+    public function RemoveUser($id) {
+        try {
             $request = 'delete from Utilisateur where id_utilisateur=:id';
             $statement = $this->getBdd()->prepare($request);
-            $statement->bindParam(':id', $_id, PDO::PARAM_INT);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
             $result = $statement->execute();
         } catch (PDOException $exception) {
             error_log('Request error: ' . $exception->getMessage());
@@ -83,4 +102,85 @@ class InterfaceBDD {
         }
         return $result;
     }
+
+    public function RequestTheme($id) {
+        try {
+            $request = 'select * from Theme where id_theme=:id';
+            $statement = $this->getBdd()->prepare($request);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+            $result = $statement->fetchAll(PDO::FETCH_CLASS, 'Theme');
+        } catch (PDOException $exception) {
+            error_log('Request error: ' . $exception->getMessage());
+            return false;
+        }
+        return $result;
+    }
+
+    public function RequestAllThemes() {
+        try {
+            $request = 'select * from Theme';
+            $statement = $this->getBdd()->prepare($request);
+            $statement->execute();
+            $result = $statement->fetchAll(PDO::FETCH_CLASS, 'Theme');
+        } catch (PDOException $exception) {
+            error_log('Request error: ' . $exception->getMessage());
+            return false;
+        }
+        return $result;
+    }
+
+    public function AddPartie($partie) {
+        try {
+            $request = 'insert into Partie(nom_partie) values(:nom_partie)';
+            $statement = $this->getBdd()->prepare($request);
+            $statement->bindParam(':nom_partie', $partie->getNom_partie(), PDO::PARAM_STR, 256);
+            $result = $statement->execute();
+        } catch (PDOException $exception) {
+            error_log('Connection error: ' . $exception->getMessage());
+            return false;
+        }
+        return $result;
+    }
+
+    public function RequestPartie($id) {
+        try {
+            $request = 'select * from Partie where id_partie=:id';
+            $statement = $this->getBdd()->prepare($request);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $statement->execute();
+            $result = $statement->fetchAll(PDO::FETCH_CLASS, 'Partie');
+        } catch (PDOException $exception) {
+            error_log('Request error: ' . $exception->getMessage());
+            return false;
+        }
+        return $result;
+    }
+
+    public function RequestAllParties($id) {
+        try {
+            $request = 'select * from Partie';
+            $statement = $this->getBdd()->prepare($request);
+            $statement->execute();
+            $result = $statement->fetchAll(PDO::FETCH_CLASS, 'Partie');
+        } catch (PDOException $exception) {
+            error_log('Request error: ' . $exception->getMessage());
+            return false;
+        }
+        return $result;
+    }
+
+    public function RemovePartie($id) {
+        try {
+            $request = 'delete from Partie where id_partie=:id';
+            $statement = $this->getBdd()->prepare($request);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+            $result = $statement->execute();
+        } catch (PDOException $exception) {
+            error_log('Request error: ' . $exception->getMessage());
+            return false;
+        }
+        return $result;
+    }
+
 }
